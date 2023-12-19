9dd69267801e1a92a6283a46fc0b79b387978441
romantkacik <123880795+romantkacik@users.noreply.github.com>
2023-12-19 19:22:51 +0100

    ejercicio 5

---
+++ ejerc_4.js
@@ -0,0 +1,42 @@
+//* 4.1 Dado el siguiente array, devuelve un array con sus nombres utilizando .map().
+// const users = [
+//   { id: 1, name: 'Abel' },
+//   { id: 2, name: 'Julia' },
+//   { id: 3, name: 'Pedro' },
+//   { id: 4, name: 'Amanda' },
+// ]
+// const names = users.map((user) => user.name)
+// console.log(names)
+
+//* 4.2 Dado el siguiente array, devuelve una lista que contenga los valores de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que empiece por 'A'.
+const users2 = [
+  { id: 1, name: 'Abel' },
+  { id: 2, name: 'Julia' },
+  { id: 3, name: 'Pedro' },
+  { id: 4, name: 'Amanda' },
+]
+const names2 = users2.map((user) => {
+  if (user.name[0] === 'A') {
+    return 'Anacleto'
+  } else {
+    return user.name
+  }
+})
+
+console.log(names2)
+
+//* 4.3 Dado el siguiente array, devuelve una lista que contenga los valores de la propiedad .name y añade al valor de .name el string ' (Visitado)' cuando el valor de la propiedad isVisited = true.
+const cities = [
+  { isVisited: true, name: 'Tokyo' },
+  { isVisited: false, name: 'Madagascar' },
+  { isVisited: true, name: 'Amsterdam' },
+  { isVisited: false, name: 'Seul' },
+]
+const nameCities = cities.map((city) => {
+  if (city.isVisited === true) {
+    return (city.name += ' vicitado')
+  } else {
+    return city.name
+  }
+})
+console.log(nameCities)
