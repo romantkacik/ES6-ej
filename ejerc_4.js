96b5e086617787820492880a9ea5c8cfa9b900e1
romantkacik <123880795+romantkacik@users.noreply.github.com>
2023-12-20 23:25:56 +0100

    ejerc_13

--- ejerc_4.js
+++ ejerc_4.js
@@ -1,12 +1,12 @@
 //* 4.1 Dado el siguiente array, devuelve un array con sus nombres utilizando .map().
-// const users = [
-//   { id: 1, name: 'Abel' },
-//   { id: 2, name: 'Julia' },
-//   { id: 3, name: 'Pedro' },
-//   { id: 4, name: 'Amanda' },
-// ]
-// const names = users.map((user) => user.name)
-// console.log(names)
+const users = [
+  { id: 1, name: 'Abel' },
+  { id: 2, name: 'Julia' },
+  { id: 3, name: 'Pedro' },
+  { id: 4, name: 'Amanda' },
+]
+const names = users.map((user) => user.name)
+console.log(names)
 
 //* 4.2 Dado el siguiente array, devuelve una lista que contenga los valores de la propiedad .name y cambia el nombre a 'Anacleto' en caso de que empiece por 'A'.
 const users2 = [
